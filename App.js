4ba1a7d098e92cf93d6af7f7b152a46d1e67e70b
a.bellippady_ext <ananya.bellippady@capgemini.com>
2022-12-24 13:18:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const heading = React.createElement("h1",
+    {
+        id: "h1 id",
+    },
+    "Hello from React!");
+
+    const header2 = React.createElement("h2",{},"header 2 ");
+    const header3 = React.createElement("h2",{},"header 2 ");
+    const container = React.createElement("div",{id:"container"},[header2, header3])
+    console.log(heading);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    //parsing a react element inside the root
+    //root.render(heading);
+    root.render(container);
